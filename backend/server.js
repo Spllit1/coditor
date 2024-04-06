b4202fbf2614216603418ef9b4ea9fea809b9afa
Spllit1 <felixschm10@gmail.com>
2024-04-06 17:19:40 +0100

    added mor functionality; run endpoint supports input now (server side only)

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,83 @@
 const { spawn } = require('child_process');
+const { spawnSync } = require('child_process');
+
 const express = require('express')
+const fs = require('fs');
+const { readdir } = require('fs');
+
 const app = express()
 app.use(express.json());
 const port = 3000
 
-
-
-
 app.post('/api/run', (req, res) => {
     console.clear();
-    const body = req.body;
-    // run user code object
-    const rucobject = spawn('python', ['-u','-c', body.key]);
-    res.send(`Api recieved: ${JSON.stringify(body.key)}`);
-    console.log(`Running user code object: ${body.key}`)
-    rucobject.stdout.on('data', (data) => {
-        console.log(`${data}`);
+    const command = fs.readFileSync('./run.txt', 'utf8');
+    console.log('command:', command);
+    const childProcess = spawn(command, [], { shell: true});
+    childProcess.stdout.setEncoding('utf-8');
+
+    process.stdin.pipe(childProcess.stdin);
+
+
+    childProcess.stdout.on('data', (chunk) => {
+        console.log('stdout chunk:', chunk);
     });
-    
-    rucobject.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
+
+    childProcess.stderr.on('data', (chunk) => {
+        console.error(`stderr: ${chunk}`);
     });
-    
-    rucobject.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
+
+
+    childProcess.on('exit', (code, signal) => {
+        console.log('Process exited with code:', code);
+    });
+
+    // Handle process error
+    childProcess.on('error', (err) => {
+        console.error('Error executing command:', err);
     });
 });
+
+
+app.post('/api/create_file', (req, res) => {
+    const { fileName, content } = req.body;
+    const filePath = `./UserCode/${fileName}`;
+
+    fs.writeFile(filePath, content, (err) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error creating file');
+        } else {
+            res.send('File created successfully');
+        }
+    });
+});
+
+app.post('/api/get_all_content', (req, res) => {
+    const directoryPath = './UserCode';
+
+    readdir(directoryPath, (err, files) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error getting directory content');
+        } else {
+            res.send(files);
+        }
+    });
+});
+
+app.post('/api/get_directory_content', (req, res) => {
+    // TODO: implement this endpoint
+})
+
+app.post('/api/get_file_content', (req, res) => {
+
+})
+
+app.post('/api/modify_run_script', (req, res) => {
+
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- backend/testapi.js
+++ backend/testapi.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
+const fs = require('fs');
 
+/*
 const data = {
     key: `
 import time
@@ -15,4 +17,47 @@ axios.post('http://localhost:3000/api/run', data)
         console.log('Body: ', res.data);
     }).catch((err) => {
         console.error(err);
+    });*/
+
+
+/*
+const fileName = 'test.txt';
+const content = 'Hello, woraaaald!';
+
+axios.post('http://localhost:3000/api/create_file', { fileName, content })
+    .then((res) => {
+        console.log(res.data); // 'File created successfully'
+    })
+    .catch((error) => {
+        console.error(error);
+    });*/
+
+/*
+axios.post('http://localhost:3000/api/get_all_content')
+    .then((res) => {
+        console.log(res.data);
+    })
+    .catch((error) => {
+        console.error(error);
+    });*/
+
+
+axios({
+    method: 'post',
+    url: 'http://localhost:3000/api/run',
+    responseType: 'stream'
+})
+.then(function (response) {
+    response.data.on('data', (chunk) => {
+        process.stdout.write(chunk);
     });
+
+
+    response.data.on('end', () => {
+        console.log('Stream ended');
+    });
+})
+.catch(function (error) {
+    console.error(error);
+});
+    
